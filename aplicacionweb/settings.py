d117cc0eb2e8709b2b3e9b3d9b9c10e9b03c736e
Jahir <soyjahirjesua04@gmail.com>
2024-02-10 01:10:33 -0600

    Modificado settings de nuevo

--- aplicacionweb/settings.py
+++ aplicacionweb/settings.py
@@ -85,10 +85,7 @@ DATABASES = {
     #     # 'ENGINE': 'django.db.backends.sqlite3',
     #     # 'NAME': BASE_DIR / 'db.sqlite3',
     # )
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 
