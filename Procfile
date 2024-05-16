0f1466e9f719cd1c01ce2c558eb0d794e01d76b2
Jahir <soyjahirjesua04@gmail.com>
2024-05-15 19:36:51 -0600

    Deploy app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aplicacionweb.wsgi
--- aplicacionweb/settings.py
+++ aplicacionweb/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'aplicacionweb.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #      'ENGINE': 'django.db.backends.sqlite3',
-    #      'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config(conn_max_age=600)
 }
 
 
